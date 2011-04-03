51f7321da2bbf66d6ae43ade5a5e03a5896d8cf9
chi <azn.jalapeno@gmail.com>
2011-04-02 23:30:51 -0700

    cleaned up and commented, all works

--- hangman.go
+++ hangman.go
@@ -12,34 +12,6 @@ import (
 const (
   BUF_SIZ = 100
   BLANK= '*'
-
-  // leters
-  A = 0
-  B = 1
-  C = 2
-  D = 3
-  E = 4
-  F = 5
-  G = 6
-  H = 7
-  I = 8
-  J = 9
-  K = 10
-  L = 11
-  M = 12
-  N = 13
-  O = 14
-  P = 15
-  Q = 16
-  R = 17
-  S = 18
-  T = 19
-  U = 20
-  V = 21
-  W = 22
-  X = 23
-  Y = 24
-  Z = 25
 )
 
 var (
@@ -50,7 +22,6 @@ var (
 
   l_list_size int
 )
-
 
 
 type counters struct {
@@ -69,6 +40,8 @@ func main() {
 
 }
 
+// given a new word "word" we will now attempt to guess the word
+// program will try until completion
 func try_word (word string) {
   total_tries := 0
   bad_tries := 0
@@ -88,12 +61,16 @@ func try_word (word string) {
 
   for {
     l, w := pick()
+
     lg, wg := try_guess(l, w)
     if !lg {
       bad_tries++
     }
     total_tries += 1
-    updates (l, w)
+    if !updates (l, w) {
+      fmt.Printf("No such word in dictionary!\n")
+      return
+    }
 
     fmt.Printf("%2d %2d guess: %c\tleft: %6d\t", total_tries, bad_tries, l, l_list_size)
     fmt.Printf("so far: %s\n", l_so_far)
@@ -107,15 +84,16 @@ func try_word (word string) {
 
 }
 
-
-
+// based on counters and current progress, attempt to guess a new
+// letter or word
 func pick () (byte, string) {
 
+  // a pseudo randomish seed, not perfect but better then
+  // an obviously deterministic random number generater
   rand.Seed(time.Nanoseconds())
 
   max_val   := uint(0)
   max_pos   := uint(0)
-  //mod       := 8
 
   // pick the "obvious" matches, i.e., certain letters
   // are the only possible letters that can fit in a spot
@@ -125,9 +103,9 @@ func pick () (byte, string) {
 
       if l_counts.pos[i][j] == uint(l_list_size) {
         max_pos = uint(j + 65)
-        fmt.Printf("OBVIOUS!!!!\n")
+        //fmt.Printf("OBVIOUS!!!!\n")
 
-        break;
+        break
       } else {
         if l_counts.pos[i][j] != 0 {
           break
@@ -155,13 +133,8 @@ func pick () (byte, string) {
 
       // the tie breakers
       if v == max_val {
-        // pick the letter that occurs overall the most
-        if l_counts.total[i] > max_total {
-          //fmt.Printf("***SWITCH MADE\n")
-          max_val = v
-          max_pos = uint(i)
-          max_total = l_counts.total[i]
-        } else if rand.Int() % int(mod) == 0 {
+         // pick the letter randomly
+        if rand.Int() % int(mod) == 0 {
           //fmt.Printf("***RAND SELECTED\n")
           max_val   = v
           max_pos   = uint(i)
@@ -170,7 +143,14 @@ func pick () (byte, string) {
           // this number is choosen somewhat arbitrarily
           // the intention is to randomly select the new letter
           // with decreasing probability 
-          mod *= 1.8
+          mod *= 1.5
+
+        // pick the letter that occurs overall the most
+        } else if l_counts.total[i] > max_total {
+          //fmt.Printf("***SWITCH MADE\n")
+          max_val = v
+          max_pos = uint(i)
+          max_total = l_counts.total[i]
         }
       }
     }
@@ -181,8 +161,9 @@ func pick () (byte, string) {
 }
 
 
-
-func updates (l byte, w string) {
+// update counts and various other variables after a letter
+// or word is guessed
+func updates (l byte, w string) bool {
 
   if l > 0 {
     index := l - 65
@@ -204,6 +185,11 @@ func updates (l byte, w string) {
               (l_so_far[i] != l && uint8(val) == l) {
           word_removal_count(e.Value.(string))
           l_list_size--
+
+          if l_list_size == 0 {
+            return false
+          }
+
           l_lis.Remove(e)
           e = e_prev
           break
@@ -215,13 +201,17 @@ func updates (l byte, w string) {
   } else {
 
   }
+
+  return true
 }
 
-
+// update the counters when a single word 'w' is stripped 
 func word_removal_count(w string) {
   var temp [26]uint
   var index int
 
+  // iterate across all letters in w
+  // for each letter update the counters
   for pos, v := range w {
     index = v - 65
     temp[index] = 1
@@ -241,6 +231,10 @@ func word_removal_count(w string) {
 
 }
 
+// evaluate the status of the guess that uses
+// either the character 'l' or the word 'w'
+// return:
+//    success of letter guess, overall success (i.e if word is completed)
 func try_guess (l byte, w string) (bool, bool) {
   l_match := false
 
@@ -259,9 +253,12 @@ func try_guess (l byte, w string) (bool, bool) {
    }
  }
 
+
  return true, true
 }
 
+
+// generate counters initially
 func char_count(word_len int) {
   var temp [26]uint
 
@@ -289,7 +286,8 @@ func char_count(word_len int) {
 
 }
 
-
+// initial build, scan the dictionary and bring into 
+// main memory all the words of matching length
 func file_scan(name string, word_len int)  {
   l_list_size = 0
   l_lis     = new(list.List)
